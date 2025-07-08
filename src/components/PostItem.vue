b2145c42d701b4eec0f2503f5d7e0e8df063ac00
Arthur-P-V <apetersonveatch@gmail.com>
2025-07-08 15:08:51 -0500

    starts to create a post card

---
+++ src/components/PostItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="card" style="width:fit-content; height:fit-content;">
+    <img class="card-image-top" src="../assets/logo.svg" alt="" style="width:50%;">
+    <div class="card-body">
+      <div class="h3 card-title">Lorem ipsum</div>
+      <div class="card-text">dolor sit amet ffa;ljgnas;ldgknva;sdkgn;adkng;sdk;alkdngas</div>
+    </div>
+  </div>
+</template>
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import PostItem from '@/components/PostItem.vue';
+
+</script>
+
 <template>
-  <div class="posts bg-carrib">
-    <div>Hiiiii</div>
+  <div class="posts bg-carrib d-flex justify-content-center">
+    <PostItem class="bg-tea"/>
   </div>
 
 </template>
