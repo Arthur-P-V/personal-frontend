db68c46474b8b104940befb87e3758cdaa6eeeb5
Arthur-P-V <apetersonveatch@gmail.com>
2025-07-18 17:00:29 -0500

    Fixes padding and image scaling

--- src/components/FooterIterm.vue
+++ src/components/FooterIterm.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="footer">
-    <div class="container-fluid">
-      <div class="row">
-        <col-4><a href="https://bsky.app/profile/arthurpv.bsky.social"></a></col-4>
-        <col-4></col-4>
-        <col-4></col-4>
-        <col-4></col-4>
-      </div>
+  <div class="footer container-fluid p-0">
+    <div class="row">
+      <col-4><a href="https://bsky.app/profile/arthurpv.bsky.social"></a></col-4>
+      <col-4></col-4>
+      <col-4></col-4>
+      <col-4></col-4>
     </div>
   </div>
 </template>
--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="card w-25">
-    <img class="card-image-top" src="../assets/logo.svg" alt="" />
+  <div class="card">
+    <img class="card-image-top img-fluid" src="../assets/logo.svg" alt="" />
     <div class="card-body">
       <div class="h3 card-title">Lorem ipsum</div>
       <div class="card-text">dolor sit amet ffa;ljgnas;ldgknva;sdkgn;adkng;sdk;alkdngas</div>
